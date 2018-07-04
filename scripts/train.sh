fa3f1d4c8939343e58220d708058d4949a88b75f
gaohaidong <gaohaidong@zju.edu.cn>
2018-07-04 19:48:38 +0800

    Update train.sh

--- scripts/train.sh
+++ scripts/train.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 
 python2 tools/train_net.py \
-    --cfg configs/traffic_cfgs/e2e_faster_rcnn_X-101-32x8d-FPN_2x.yaml \
-    OUTPUT_DIR trained_models/e2e_faster_rcnn_X-101-32x8d-FPN_2x/
+    --cfg configs/traffic_cfgs/e2e_faster_rcnn_X-101-64x4d-FPN_roi_conv.yaml \
+    OUTPUT_DIR trained_models/e2e_faster_rcnn_X-101-64x4d-FPN_roi_conv/
