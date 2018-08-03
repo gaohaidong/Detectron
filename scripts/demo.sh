5eb5460a0dbefe43a19af5f6267b4759e26298a4
tx <tt>
2018-08-03 21:27:33 +0800

    Get tianchi results from croped patches

--- scripts/demo.sh
+++ scripts/demo.sh
@@ -1,10 +1,10 @@
 
 #!/usr/bin/env bash
-export CUDA_VISIBLE_DEVICES=0
+export CUDA_VISIBLE_DEVICES=1
 python2 tools/infer_simple.py \
     --cfg configs/cloth_cfgs/e2e_faster_rcnn_X-101-64x4d-FPN.yaml \
     --output-dir val-visualizations \
     --image-ext jpg \
-    --csv_res cloth-x101-crop.csv \
-    --wts trained_models/e2e_faster_rcnn_X-101-64x4d-FPN/train/cloth_train/generalized_rcnn/model_iter129999.pkl \
-    /data/02Cloth/crop_test/
+    --csv_res cloth-x101-crop-test-bb.csv \
+    --wts trained_models/e2e_faster_rcnn_X-101-64x4d-FPN/train/cloth_train/generalized_rcnn/model_final.pkl \
+    /data/02Cloth/crop_test_bb/
--- tools/get_cloth_res.py
+++ tools/get_cloth_res.py
@@ -1,5 +1,5 @@
-res_csv = 'cloth-x101-crop.csv'
-tianchi_csv = 'cloth-x101-crop-tc.csv'
+res_csv = 'cloth-x101-crop-test-b.csv'
+tianchi_csv = 'cloth-x101-crop-tc-tx.csv'
 im_threshs = dict()
 with open(res_csv) as f:
     for line in f.readlines():
@@ -14,14 +14,42 @@ with open(res_csv) as f:
                 if box == '':
                     continue
                 bbox = map(float, box.split('_'))
-                if patch_id % 3 != 0 and bbox[1] + bbox[3] < 320:
+
+                if patch_id % 3 != 0 and bbox[1] + bbox[3] < 160:
                     continue
-                if patch_id >= 3 and bbox[0] + bbox[2] < 420:
+                if patch_id >= 3 and bbox[0] + bbox[2] < 213:
                     continue
-                if patch_id % 3 != 2 and bbox[1] > 320:
+                if patch_id % 3 != 2 and bbox[1] > 160:
                     continue
-                if patch_id < 6 and bbox[0] > 420:
+                if patch_id < 6 and bbox[0] > 1067:
                     continue
+                '''
+                H1, W1 = 1920/2, 2560/2
+                HT = 1920 / 12
+                WT = 2560 / 12
+                x1, y1 = bbox[0], bbox[1]
+                x2 = x1 + bbox[2] - 1
+                y2 = y1 + bbox[3] - 1
+                if patch_id % 3 == 0:
+                    if y1 > H1 - HT:
+                        continue
+                if patch_id % 3 == 1:
+                    if y2 < HT or y1 > H1 - HT:
+                        continue
+                if patch_id % 3 == 2:
+                    if y2 < HT:
+                        continue
+
+                if patch_id / 3 == 0:
+                    if x1 > W1 - WT:
+                        continue
+                if patch_id / 3 == 1:
+                    if x2 < WT or x1 > W1 - WT:
+                        continue
+                if patch_id / 3 == 2:
+                    if x2 < WT:
+                        continue
+                '''
                 threshs.append(bbox[-1])
             im = items[0][:items[0].rfind('_')]
             if im not in im_threshs:
@@ -34,7 +62,7 @@ with open(tianchi_csv, 'w') as fw:
         threshs = im_threshs[im]
         if threshs == []:
             threshs.append(0.000001)
-        fw.write('\n{},{}'.format(im, round(max(threshs), 6)))
+        fw.write('\n{}.jpg,{}'.format(im, round(max(threshs), 6)))
 
 
 
